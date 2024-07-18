42daff9d4e3179bc0c8f0bb0b0ef685a47f3eb56
Isadora Piropo <168051407+IsadoraPiropop@users.noreply.github.com>
2024-07-17 23:30:01 -0300

    Add files via upload

---
+++ BEE1003.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<clocale>
+#include<cmath>
+#include<iomanip>
+using namespace std;
+    int main () {
+    setlocale (LC_ALL, "Portuguese");
+    int A, B, SOMA;
+    cin>>A>>B;
+    SOMA=A+B;
+    cout<<"SOMA = "<<SOMA<<endl;
+        
+        
+        return 0;
+    }
